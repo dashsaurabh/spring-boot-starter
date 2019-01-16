bee33985b9efbd1d0274c6c3c046c8ff5dd04ca5
Saurabh <dashsaurabh@gmail.com>
2019-01-16 18:54:09 +0530

    Enabling Spring Boot Starter Data Rest

--- src/main/java/com/progressivecoder/demo/springbootstarter/repositories/VehicleRepository.java
+++ src/main/java/com/progressivecoder/demo/springbootstarter/repositories/VehicleRepository.java
@@ -2,8 +2,14 @@ package com.progressivecoder.demo.springbootstarter.repositories;
 
 import com.progressivecoder.demo.springbootstarter.entities.Vehicle;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.UUID;
 
+@RepositoryRestResource(collectionResourceRel = "cars", path = "cars")
 public interface VehicleRepository extends CrudRepository<Vehicle, UUID> {
+
+    List<Vehicle> findByMake(@Param("make") String make);
 }
